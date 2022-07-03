db8a2274083d21277df974d6e724ba2447de44cc
kupuk23 <kupuk23@gmail.com>
2022-07-03 17:58:02 +0700

    PassingVariable

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -25,21 +25,15 @@ class ResultsPage extends StatefulWidget {
 }
 
 class _ResultsPageState extends State<ResultsPage> {
-  int faset1Score = 1;
-  int faset2Score = 4;
-  int faset3Score = 6;
-  int faset4Score = 2;
-  int faset5Score = 8;
-  int faset6Score = 2;
-  int faset7Score = 1;
-  int faset8Score = 9;
-  int faset9Score = 8;
+  String name = "";
   int values = 10;
   int dom1 = 1;
   int dom2 = 3;
   int dom3 = 6;
   int dom4 = 9;
-
+  List<int> stdScore = [];
+  List<int> faset2 = [];
+  final db = FirebaseFirestore.instance;
   GlobalKey _tableKey = GlobalKey();
   late double tableWidth;
   late double tableHeight;
@@ -53,26 +47,44 @@ class _ResultsPageState extends State<ResultsPage> {
     });
   }
 
+  Stream<List> getData() async* {
+    db.collection('users2').get().then((QuerySnapshot querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        if (doc['id'] == int.tryParse(widget.resultId)) {
+          // faset.add(int.parse(doc['id']));
+          try {
+            stdScore.add(doc['stdScore1']);
+            stdScore.add(doc['stdScore1']);
+            stdScore.add(doc['stdScore2']);
+            stdScore.add(doc['stdScore3']);
+            stdScore.add(doc['stdScore4']);
+            stdScore.add(doc['stdScore5']);
+            stdScore.add(doc['stdScore6']);
+            stdScore.add(doc['stdScore7']);
+            stdScore.add(doc['stdScore8']);
+            stdScore.add(doc['stdScore9']);
+            print('faset is : $stdScore');
+            break;
+          } catch (err) {
+            print("faset err is $err");
+            // faset.clear();
+          }
+        }
+      }
+    });
+    print("faset before out $stdScore");
+    yield stdScore;
+  }
+
   @override
   void initState() {
     super.initState();
+    this.getData();
     print('ID IS ${widget.resultId}');
   }
 
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    List<int> faset = [];
-
-    db.collection('diaries').get().then((QuerySnapshot querySnapshot) {
-      for (var doc in querySnapshot.docs) {
-        if (doc['id'] == '12') {
-          faset.add(int.parse(doc['id']));
-          print(faset);
-        }
-      }
-    });
-
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementfasetScore method above.
     //
@@ -98,31 +110,76 @@ class _ResultsPageState extends State<ResultsPage> {
                 // Center is a layout widget. It takes a single child and positions it
                 // in the middle of the parent.
                 child: Container(
-              margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-              color: Color.fromARGB(255, 220, 189, 189),
-              height: MediaQuery.of(context).size.height * 0.95, // - 500,
-              width: MediaQuery.of(context).size.width * 0.95, //1920 - 500,
-              child: Container(
-                margin: EdgeInsets.all(10),
-                color: Colors.purple.withOpacity(0.1),
-                height: MediaQuery.of(context).size.height * 0.9, // - 500,
-                width: MediaQuery.of(context).size.width * 0.9, //1920 - 500,
-                child: FasetChart(
-                    faset1Score: faset1Score,
-                    faset2Score: faset2Score,
-                    faset3Score: faset3Score,
-                    faset4Score: faset4Score,
-                    faset5Score: faset5Score,
-                    faset6Score: faset6Score,
-                    faset7Score: faset7Score,
-                    faset8Score: faset8Score,
-                    faset9Score: faset9Score,
-                    dom1: dom1,
-                    dom2: dom2,
-                    dom3: dom3,
-                    dom4: dom4),
-              ),
-            )),
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    color: Color.fromARGB(255, 220, 189, 189),
+                    height: MediaQuery.of(context).size.height * 1.5, // - 500,
+                    width:
+                        MediaQuery.of(context).size.width * 0.95, //1920 - 500,
+                    child: StreamBuilder<QuerySnapshot>(
+                        stream: db.collection('users2').snapshots(),
+                        builder:
+                            (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (!snapshot.hasData) {
+                            return Center(
+                                child:
+                                    Center(child: CircularProgressIndicator()));
+                          }
+                          for (var doc in snapshot.data!.docs) {
+                            if (doc['id'] == int.tryParse(widget.resultId)) {
+                              // faset.add(int.parse(doc['id']));
+                              try {
+                                name = doc['name'];
+                                stdScore.add(doc['stdscore1']);
+                                stdScore.add(doc['stdscore1']);
+                                stdScore.add(doc['stdscore2']);
+                                stdScore.add(doc['stdscore3']);
+                                stdScore.add(doc['stdscore4']);
+                                stdScore.add(doc['stdscore5']);
+                                stdScore.add(doc['stdscore6']);
+                                stdScore.add(doc['stdscore7']);
+                                stdScore.add(doc['stdscore8']);
+                                stdScore.add(doc['stdscore9']);
+                                stdScore.add(doc['stdscore10']);
+                                stdScore.add(doc['stdscore11']);
+                                stdScore.add(doc['stdscore12']);
+                                stdScore.add(doc['stdscore13']);
+                                stdScore.add(doc['stdscore14']);
+                                stdScore.add(doc['stdscore15']);
+                                print('faset is : $stdScore');
+                                break;
+                              } catch (err) {
+                                print("faset err is $err");
+                                stdScore.clear();
+                              }
+                            } else {
+                              stdScore.clear();
+                            }
+                          }
+                          if (stdScore.isEmpty) {
+                            return Container(
+                              child: Text("no Data"),
+                            );
+                          }
+                          return Container(
+                            margin: EdgeInsets.all(10),
+                            color: Colors.purple.withOpacity(0.1),
+                            height: MediaQuery.of(context).size.height *
+                                0.9, // - 500,
+                            width: MediaQuery.of(context).size.width *
+                                0.9, //1920 - 500,
+                            child: Column(
+                              children: [
+                                Text('Name :  $name'),
+                                FasetChart(
+                                    fasetScore: stdScore,
+                                    dom1: dom1,
+                                    dom2: dom2,
+                                    dom3: dom3,
+                                    dom4: dom4),
+                              ],
+                            ),
+                          );
+                        }))),
           ),
         ),
       ),
@@ -133,30 +190,14 @@ class _ResultsPageState extends State<ResultsPage> {
 class FasetChart extends StatelessWidget {
   const FasetChart({
     Key? key,
-    required this.faset1Score,
-    required this.faset2Score,
-    required this.faset3Score,
-    required this.faset4Score,
-    required this.faset5Score,
-    required this.faset6Score,
-    required this.faset7Score,
-    required this.faset8Score,
-    required this.faset9Score,
+    required this.fasetScore,
     required this.dom1,
     required this.dom2,
     required this.dom3,
     required this.dom4,
   }) : super(key: key);
 
-  final int faset1Score;
-  final int faset2Score;
-  final int faset3Score;
-  final int faset4Score;
-  final int faset5Score;
-  final int faset6Score;
-  final int faset7Score;
-  final int faset8Score;
-  final int faset9Score;
+  final List<int> fasetScore;
   final int dom1;
   final int dom2;
   final int dom3;
@@ -168,39 +209,63 @@ class FasetChart extends StatelessWidget {
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset1Score),
+      rowTable(fasetScore: fasetScore[0]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset2Score),
+      rowTable(fasetScore: fasetScore[1]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset3Score),
+      rowTable(fasetScore: fasetScore[2]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset4Score),
+      rowTable(fasetScore: fasetScore[3]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset5Score),
+      rowTable(fasetScore: fasetScore[4]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset6Score),
+      rowTable(fasetScore: fasetScore[5]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset7Score),
+      rowTable(fasetScore: fasetScore[6]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset8Score),
+      rowTable(fasetScore: fasetScore[7]),
       SizedBox(
         height: 20,
       ),
-      rowTable(fasetScore: faset9Score),
+      rowTable(fasetScore: fasetScore[8]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[9]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[10]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[11]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[12]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[13]),
+      SizedBox(
+        height: 20,
+      ),
+      rowTable(fasetScore: fasetScore[14]),
       SizedBox(
         height: 100,
       ),
